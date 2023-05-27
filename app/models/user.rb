c804716ed12c80b44a77ccc43d640e3223567286
atifjavaid <atifjavaid267@gmail.com>
2023-05-27 18:06:13 +0500

    role defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :name, format: { with: /\A[a-zA-Z ]+\z/, message: 'only allows letters and spaces' }
+
+  def admin?
+    role == 'admin'
+  end
+
+  def member?
+    role == 'member'
+  end
 end
