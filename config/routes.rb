e951ac64a6616b7348f56f6ec2a8aa07ef9c6535
atif <atifjavaid267@gmail.com>
2023-06-18 04:22:24 +0500

    search code added

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,9 @@ Rails.application.routes.draw do
     member do
       get :view_remedies
     end
+    collection do
+      get 'search'
+    end
   end
 
   resources :remedies, only: %i[new create index destroy]
