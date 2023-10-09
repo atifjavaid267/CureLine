cb91c6dec1f79b0dfac1e6fd1cd64381e715f2de
atif <atifjavaid267@gmail.com>
2023-10-09 12:59:16 +0500

    all files updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,6 @@
-# frozen_string_literal: true
-
-# class User
 class User < ApplicationRecord
-  has_many :diseases
-  has_many :remedies
-  has_many :health_concerns
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  validates :name, format: { with: /\A[a-zA-Z ]+\z/, message: 'only allows letters and spaces' }
-
-  enum role: { admin: 0, member: 1 }
 end
