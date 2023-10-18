fa903babe42e1224545a0c6afc03e82963bf476a
atif <atifjavaid267@gmail.com>
2023-10-18 15:37:03 +0500

    version changed to rails 7.0.8

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
-  devise_scope :user do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
-  root "home#index"
-  resources :home, only: :index
-
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
--- db/migrate/20231010071652_add_name_and_role_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddNameAndRoleToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :name, :string
-    add_column :users, :role, :integer, default: 1
-  end
-end
